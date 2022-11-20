88a4e58d3fe50197a8735ffde129592ff9f3096f
C0dingWithT0ny <78626437+C0dingWithT0ny@users.noreply.github.com>
2022-11-20 12:17:58 -0800

    commiting max's changes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home';
-import StakeList from './StakeList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StakeList/>
+    <Home/>
   </React.StrictMode>
 );
 
