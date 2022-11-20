b1a0402bacb6f20a7e6cd665fb23c10412fa4eb1
C0dingWithT0ny <78626437+C0dingWithT0ny@users.noreply.github.com>
2022-11-20 11:20:07 -0800

    message

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home';
+import StakeList from './StakeList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home/>
+    <StakeList/>
   </React.StrictMode>
 );
 
