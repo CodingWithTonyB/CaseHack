d24fe5fffb5bcc18c77ce4f5b454df7569132ea5
Henry N <hnemail1000@gmail.com>
2022-11-21 10:34:12 -0800

    Latest version -Fixed Bug First Render.

--- src/StakeList.js
+++ src/StakeList.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 import {supabase} from "./supabase";
 import { Link } from "react-router-dom";
 import './Home.css'
@@ -9,56 +9,46 @@ const tableHeaders = ["Name of the Pool", "ETH Staked", "Number of Node Operator
  // const array = [["Rocket Pool", "307,040", "1,739", "4.64", "https://rocketpool.net/#header"],
  //     ["StakeWise", "67,488.70", "2,099", "5.28", "https://app.stakewise.io/"]]
 
- let array = [];
 
 function StakeList() {
-
+  const [myData, setMyData] = useState([]);
+  const getData = async () => {
+    try {
+        const {data, error} = await supabase
+            .from('posts')
+            .select('pool_name, eth_staked, nodeoperators, apr, link')
+        if (error) {
+            throw new Error("Error!")
+        }
+        else{
+        console.log(data)
+        return data;
+        }
+    }
+    catch(error) {
+        console.log("error");
+    }
+  }
 const [arrowState, setArrowState] = useState("glyphicon glyphicon-triangle-top");
-const [data, setData] = useState(array);
-
 const sort = (index) => {
   if (arrowState === "glyphicon glyphicon-triangle-bottom"){
       console.log(index);
-      array.sort((a,b) => a[index].localeCompare(b[index].toUpperCase()));
+      myData.sort((a,b) => a[index].localeCompare(b[index].toUpperCase()));
     setArrowState("glyphicon glyphicon-triangle-top")
   }
   else{
       console.log(index);
-      array.sort((a,b) => a[index].localeCompare(b[index].toUpperCase()));
-      array.reverse();
+      myData.sort((a,b) => a[index].localeCompare(b[index].toUpperCase()));
+      myData.reverse();
     setArrowState("glyphicon glyphicon-triangle-bottom")
   }
 }
-
+useEffect(() => getData().then(e => setMyData(e.map(Object.values))), []);
 const makeArrow = (index) => {
     if (index !== 4) {
         return <span id={index} className={arrowState} onClick={() => sort(index)}></span>
     }
 }
-
-const loadData = async () => {
-  try {
-      const {data, error} = await supabase
-          .from('posts')
-          .select('pool_name, eth_staked, nodeoperators, apr, link')
-      if (error) {
-          throw new Error("Error!")
-      }
-      return data;
-  }
-  catch(error) {
-      console.log("error");
-  }
-}
-let fetchResult = loadData();
-fetchResult.then(param => {
-  console.log(param);
-for (let x in param){
-  let elements = param[x];
-  console.log(Object.values(elements));
-  array.push(Object.values(elements))      
-}
-})
     return (
        <div className="App h-screen">
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"></link>
@@ -88,8 +78,8 @@ for (let x in param){
           </tr>
 
         </thead>
-        <tbody class='styled-table'>
-          {data.map(e => (<tr class='color'>{e.map((e, index) => index === 0 ? (<td class='color'scope="row"><b>{e}</b></td>): 
+        <tbody className='styled-table'>
+          {myData.map(e => (<tr class='color'>{e.map((e, index) => index === 0 ? (<td class='color'scope="row"><b>{e}</b></td>): 
           (index === 4? 
           (<td scope="row"><a href={e}>{e}</a></td>)
           : (<td scope="row">{e}</td>)))}</tr>)
